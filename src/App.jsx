1817e26baae1d96a2f3e42b478e25c76a88775e8
Multiprocesssing-CODER <sagnik.dey745@icloud.com>
2024-10-20 22:20:46 +0530

    Added the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,41 @@
-import React from 'react'
+import CustomerReviews from "./components/CustomerReviews";
+import Footer from "./components/Footer";
+import PopularProducts from "./components/PopularProducts";
+import Hero from "./components/Hero";
+import SuperQuality from "./components/SuperQuality";
+import Services from "./components/Services";
+import SpecialOffer from "./components/SpecialOffer";
+import Subscribe from "./components/Subscribe";
+import Navbar from "./components/Navbar";
 
 const App = () => (
-  <main className='relative'>
-    Nav 
-    <section className='xl:padding-1 wide:padding-r padding-b'>
-      Hero 
+  <main className="relative">
+    <Navbar />
+    <section className="xl:padding-1 wide:padding-r padding-b">
+      <Hero />
     </section>
-    <section className='padding'>
-      Popular 
+    <section className="padding">
+      <PopularProducts />
     </section>
-    <section className='padding'>
-      Super Quality 
+    <section className="padding">
+      <SuperQuality />
     </section>
-    <section className='padding-x py-10'>
-      Services 
+    <section className="padding-x py-10">
+      <Services />
     </section>
-    <section className='padding'>
-      Special Offer 
+    <section className="padding">
+      <SpecialOffer />
     </section>
-    <section className='bg-pale-blue padding'>
-      Customer Reviews 
+    <section className="bg-pale-blue padding">
+      <CustomerReviews />
     </section>
-    <section className='padding-x sm:py-32 py-16 w-full'>
-      Subscribe 
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe />
     </section>
-    <section className='bg-black padding-x padding-t pb-8'>
-      Footer
+    <section className="bg-black padding-x padding-t pb-8">
+      <Footer />
     </section>
   </main>
-)
+);
 
-export default App
+export default App;
